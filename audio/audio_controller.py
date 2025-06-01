f26547087ad98c02605706eb4b27f12eb38d12dd
neoguojing <guojing_neo@163.com>
2025-06-01 11:03:45 +0800

    add lock for player and recorder

--- audio/audio_controller.py
+++ audio/audio_controller.py
@@ -1,26 +1,38 @@
 import asyncio
 from .audio_play import AudioPlayer
 from .audio_record import ContinuousAudioListener
+
 class AudioControllerAsync:
     def __init__(self):
         self.lock = asyncio.Lock()
         self.recorder = ContinuousAudioListener()
         self.recorder.start()
         self.player = AudioPlayer()
-
+    
+    async def run(self):
+        asyncio.create_task(self.player.consumer())
+        
     async def record(self):
         async with self.lock:
             print("开始录音")
             clip = await self.recorder.record()
-            print("录音结束")
             return clip
 
     async def play(self,url):
         async with self.lock:
             print("开始播放")
-            await self.player.play(url)
+            await self.player.producer(url)
             print("播放结束")
-            
+    
+    def get_buffered_data(self, seconds: int):
+        return self.recorder.get_buffered_data(seconds)
+        
+    def get_recorder_param(self):
+        return self.recorder.channels,self.recorder.rate,self.recorder.get_sampwidth()
+        
+    def is_silence(self,frame: bytes):
+        return self.recorder.is_silence(frame)
+        
     def terminate(self):
         self.recorder.stop()
         self.player.terminate()
