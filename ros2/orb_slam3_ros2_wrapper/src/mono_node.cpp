55b6c01ea8c7e6b92b140f614a8e52f1884c20aa
guoerjun <guoerjun@sensetime.com>
2025-12-16 11:56:52 +0800

    update

--- ros2/orb_slam3_ros2_wrapper/src/mono_node.cpp
+++ ros2/orb_slam3_ros2_wrapper/src/mono_node.cpp
@@ -1,32 +1,5 @@
 #include "orb_slam3_ros2_wrapper/SystemWrapper.hpp"
 
-// 构造函数实现 (SystemWrapper::SystemWrapper)
-// ... [在此处实现 ORB_SLAM3::System 的初始化和参数读取]
-
-// 单目回调函数实现
-void SystemWrapper::mono_callback(const sensor_msgs::msg::Image::SharedPtr msg)
-{
-    // 1. 转换图像格式
-    cv_bridge::CvImagePtr cv_ptr;
-    try {
-        cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::MONO8);
-    } catch (cv_bridge::Exception& e) {
-        RCLCPP_ERROR(this->get_logger(), "cv_bridge exception: %s", e.what());
-        return;
-    }
-    
-    // 2. 调用 ORB-SLAM3 核心函数
-    // 注意：时间戳转换
-    double timestamp = msg->header.stamp.sec + msg->header.stamp.nanosec * 1e-9;
-    cv::Mat Tcw = slam_system_->TrackMonocular(cv_ptr->image, timestamp);
-
-    // 3. 发布结果 (如果跟踪成功)
-    if (!Tcw.empty()) {
-        // Tcw 是世界坐标系到相机的变换矩阵，需要转换为 ROS 的 Pose 或 TF
-        // this->publish_pose(Tcw); 
-        // this->publish_tf(Tcw);
-    }
-}
 
 int main(int argc, char * argv[])
 {
