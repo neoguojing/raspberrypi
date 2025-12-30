3268353040bae9ca1c086d28410f3225fedfabc4
guoerjun <guoerjun@sensetime.com>
2025-12-30 19:12:53 +0800

    up

--- robot_starter.sh
+++ robot_starter.sh
@@ -3,25 +3,13 @@ set -e
 
 echo "[INFO] starting robot stack..."
 
-# ===== 2. Zenoh bridge (client) =====
-echo "[INFO] starting zenoh bridge..."
-zenoh-bridge-ros2dds --rest-http-port 25000 --namespace rt
-# zenoh-bridge-ros2dds --connect tcp/127.0.0.1:7447 client&
-
-ZENOH_PID=$!
-sleep 1
-
 # ===== 3. Foxglove bridge =====
 echo "[INFO] starting foxglove bridge..."
 ros2 launch foxglove_bridge foxglove_bridge_launch.xml &
 
-FOXGLOVE_PID=$!
 sleep 1
 
-echo "[OK] all processes started"
+# ===== 2. Zenoh bridge (client) =====
+echo "[INFO] starting zenoh bridge..."
+zenoh-bridge-ros2dds --rest-http-port 25000 --namespace rt
 
-# ===== 4. graceful shutdown =====
-trap "echo '[STOP] shutting down...'; kill $ZENOH_PID $FOXGLOVE_PID; exit 0" SIGINT SIGTERM
-
-# ===== 5. block =====
-wait
